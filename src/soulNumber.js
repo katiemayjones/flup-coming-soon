a7884dca1cfb18215255544a5457b62601c65bda
katiemayjones <katiemayjones1990@gmail.com>
2023-08-27 13:27:18 +0100

    Added notes for vowel values in JS

--- src/soulNumber.js
+++ src/soulNumber.js
@@ -1,3 +1,9 @@
 let vowels = ["a", "e", "i", "o", "u"];
 
 //add vowels together
+
+let lettersOne = ["a"];
+let lettersThree = ["u"];
+let lettersSix = ["o"];
+let lettersSeven = ["y"];
+let lettersNine = ["i"];
